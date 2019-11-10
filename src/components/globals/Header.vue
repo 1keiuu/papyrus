311e6e6109486725fb55f3fbb3cf3ec222248dc4
Ikkei Harashima <ikkei12.h@gmail.com>
2019-11-10 11:26:58 +0900

    差分調整

---
+++ src/components/globals/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <div class="align-center font-weight-light" style="font-size: 25px">Papyrus</div>
+      <v-spacer></v-spacer>
+      <v-avatar color="primary" size="45">
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+      </v-avatar>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  components: {},
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+<style scoped>
+
+</style>
