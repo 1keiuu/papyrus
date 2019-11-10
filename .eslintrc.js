c9f332792066ccde53fa11e5d1f120e2e37fef0f
Ikkei Harashima <ikkei12.h@gmail.com>
2019-11-10 11:39:50 +0900

    最終Lint調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
-    node: true
+    node: true,
   },
-  extends: 'standard',
-  globals: {
-    __static: true
-  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
   rules: {
-    semi: 0,
-    quotes: 0,
+    'import/extensions':0,
+    'semi':0,
+    'quotes': 0,
     'comma-spacing': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
@@ -24,5 +20,19 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
     'no-unused-vars': 1
-  }
-}
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
