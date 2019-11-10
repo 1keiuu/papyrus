c7662fa3380e2403b1a426063a859fbfcade7105
Ikkei Harashima <ikkei12.h@gmail.com>
2019-11-10 11:31:38 +0900

    lint調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/extensions':0,
-    'semi':0,
-    'no-console':0,
-    'global-require':0,
-  },
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint',
+    sourceType: 'module'
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-};
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'standard',
+  globals: {
+    __static: true
+  },
+  rules: {
+    semi: 0,
+    quotes: 0,
+    'comma-spacing': 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0,
+    'no-unused-vars': 1
+  }
+}
