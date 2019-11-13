4de3031ebacca0a9d7686d95215e375769e6bb1c
棚田森羅 <>
2019-11-13 22:05:58 +0900

    login画面フォーム変更

--- src/components/pages/LoginPage.vue
+++ src/components/pages/LoginPage.vue
@@ -1,21 +1,37 @@
 <template>
   <v-app class="b">
-    <v-content class="body">
+    <v-content class="content">
       <v-container fill-height>
         <v-layout row wrap justify-end="true">
           <v-flex xs5 class="py-6">
             <div>
               <div class="text-center">
-                <v-sheet color="rgba(176, 182, 182, 0.42)" min-width="420" min-height="600">
+                <v-sheet color="rgba(176, 182, 182, 0.42)" min-width="420" min-height="550">
                   <v-form ref="form" v-model="valid" lazy-validation class="form">
-                    <v-text-field v-model="email"
-                    label="ユーザー名" required></v-text-field>
-                    <v-text-field v-model="password"
-                    :rules="passwordRules" label="パスワード" required></v-text-field>
-                    <v-checkbox v-model="checkbox" label="ログイン状態を保持する" required></v-checkbox>
+                    <v-flex px-12 py-12 my-7>
+                    <v-text-field
+                      v-model="email"
+                      label="メールアドレス"
+                      :rules="emailRules"
+                      required
+                    ></v-text-field>
+                    </v-flex>
+                    <v-flex px-12>
+                    <v-text-field
+                      v-model="password"
+                      :type="show1 ? 'text' : 'password'"
+                      :rules="passwordRules"
+                      label="パスワード"
+                    ></v-text-field>
+                    <v-checkbox
+                      v-model="checkbox"
+                      label="ログイン状態を保持する"
+                      required
+                    ></v-checkbox>
+                    </v-flex>
                   </v-form>
-                  <v-flex xs10 mx-12 px-12 align-self-center="true">
-                    <v-btn class="info" min-width="200" @click="emailLogin">ログイン</v-btn>
+                  <v-flex  align-self-center="true" py-12 my-10>
+                    <v-btn class="info" min-width="324" @click="emailLogin">ログイン</v-btn>
                   </v-flex>
                 </v-sheet>
               </div>
@@ -28,57 +44,64 @@
 </template>
 
 <script>
-import firebase from 'firebase/app'
-import router from '@/router'
+import firebase from "firebase/app";
+import router from "@/router";
 
 export default {
   data: () => ({
     valid: true,
-    email: '',
-    password: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-    ],
+    email: "",
+    emailRules: [v => !!v || "E-mail is required", v => /.+@.+/.test(v) || "E-mail must be valid"],
+    password: "",
+    passwordRules: {
+      required: value => !!value || "Required.",
+      min: v => v.length >= 8 || "Min 8 characters",
+      emailMatch: () => "The email and password you entered don't match"
+    },
     select: null,
     checkbox: false,
-    lazy: false,
+    lazy: false
   }),
+
   methods: {
     validate() {
       if (this.$refs.form.validate()) {
-        this.snackbar = true
+        this.snackbar = true;
       }
     },
     reset() {
-      this.$refs.form.reset()
+      this.$refs.form.reset();
     },
     resetValidation() {
-      this.$refs.form.resetValidation()
+      this.$refs.form.resetValidation();
     },
     emailLogin() {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(result => {
-        console.log(result)
-        router.push('/home')
-      }).catch(error => {
-        console.log(error)
-        this.errorMessage = error.message
-        this.showError = true
-        alert(this.errorMessage)
-      })
-    },
-  },
-}
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(result => {
+          console.log(result);
+          router.push("/home");
+        })
+        .catch(error => {
+          console.log(error);
+          this.errorMessage = error.message;
+          this.showError = true;
+          alert(this.errorMessage);
+        });
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-.body {
-  background-image: url('../../assets/login_back.jpg');
+.content {
+  background-image: url("../../assets/login_back.jpg");
   width: 100%;
   height: 100%;
 }
 
 .form {
-  width: 70%;
+  width: 100%;
 }
 </style>
